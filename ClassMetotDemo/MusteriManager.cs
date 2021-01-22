8ceb26f03f2ac54c2b0ff248cc28cd3d8eb50d81
EserOmer <48418294+eser2928@users.noreply.github.com>
2021-01-22 20:32:19 +0300

    .

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -6,6 +6,21 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Musteri Eklendi : " + musteri.Ad);
+        }
+        
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri1 in musteriler)
+            {
+                Console.WriteLine(musteri1.Ad);
+                Console.WriteLine(musteri1.Soyad);
+                Console.WriteLine(musteri1.Yas);
+                Console.WriteLine("------------------");
 
+            }
+        }
     }
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,18 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri = new Musteri();
+            musteri.Ad = "Omer";
+            musteri.Soyad = "Eser";
+            musteri.Yas = 22;
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Erhan";
+            musteri2.Soyad = "Ercan";
+            musteri2.Yas = 28;
+            MusteriManager MM = new MusteriManager();
+            MM.Ekle(musteri);
+            Musteri[] Mlist = new Musteri[] { musteri, musteri2 };
+            MM.Listele(Mlist);
         }
     }
 }
